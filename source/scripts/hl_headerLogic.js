156d566419fc1eedf5fdb2a3aa19d8de9a9bed4a
Nicolas Floriano <nicolasfloriano27@gmail.com>
2024-04-22 17:27:00 -0300

    Untrack files now ignored

--- source/scripts/hl_headerLogic.js
+++
@@ -1,7 +0,0 @@
-const hl_header = document.querySelector('header.header');
-const hl_logo = hl_header.querySelector('.header-logo')
-const hl_hero =  document.querySelector('section.hero');
-
-window.addEventListener('scroll', () => {
-    this.window.scrollY > (hl_hero.clientHeight / 2) ? hl_logo.setAttribute("aria-hidden", false) : hl_logo.setAttribute("aria-hidden", true)
-})
